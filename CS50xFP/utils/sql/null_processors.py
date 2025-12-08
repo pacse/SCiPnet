66d1af85369a91ad8cfd77caedcc7415ee4b6100
pacse <68709871+pacse@users.noreply.github.com>
2025-12-08 18:19:09 +1300

    Update null_processors.py
    Added base class
    
    MTF's now have a nickname string
    
    added docstrings & validation to processors

--- CS50xFP/utils/sql/null_processors.py
+++ CS50xFP/utils/sql/null_processors.py
@@ -4,19 +4,21 @@ Helpers to process null values for bar templates
 
 from ..display.config import CLEAR_LVL_COLOURS
 from .models import Models
+from .exceptions import ArgumentError
 
 from dataclasses import dataclass
 
-
 # dataclasses for processed data
-
 
 class ProcessedData:
 
     @dataclass
-    class SCP:
-        id: int
-        name: str
+    class Base:
+        name_str: str
+
+    @dataclass
+    class SCP(Base):
+        id_str: str
         clear_lvl_str: str
         clear_lvl_id: int
 
@@ -34,27 +36,43 @@ class ProcessedData:
 
 
     @dataclass
-    class Site:
-        name_str: str
+    class Site(Base):
         director_str: str
 
 
     @dataclass
-    class MTF:
-        name_str: str
+    class MTF(Base):
+        nickname: str
         site: str
         leader_str: str
         active: str
 
 
     @dataclass
-    class User:
-        name_str: str
+    class User(Base):
         site: str
-        clearance: str
+        clearance_str: str
+        clearance_id: int
 
 
 def scp(info: Models.SCP) -> ProcessedData.SCP:
+    """
+    Processes SCP model for bar display
+
+    Parameters
+    ----------
+    info : Models.SCP
+        SCP model to process
+
+    Returns
+    -------
+    ProcessedData.SCP
+        Processed SCP data
+    """
+
+    # validation
+    if not isinstance(info, Models.SCP):
+        raise ArgumentError(info, 'Models.SCP')
 
     # === Process null values ===
     if info.secondary_class:
@@ -89,8 +107,8 @@ def scp(info: Models.SCP) -> ProcessedData.SCP:
 
     # === Return processed data ===
     return ProcessedData.SCP(
-        id = info.id,
-        name = info.name,
+        id_str = f'SCP-{info.id}',
+        name_str = f'"{info.name}"',
 
         clear_lvl_str = info.clearance_lvl.name,
         clear_lvl_id = info.clearance_lvl.id,
@@ -108,22 +126,25 @@ def scp(info: Models.SCP) -> ProcessedData.SCP:
     )
 
 
-def site(info: Models.Site) -> ProcessedData.Site:
-
-    if info.director:
-        director_str = (f'{info.director.name} '
-                        f'(ID: {info.director.id})')
-
-    else:
-        director_str = '[DATA EXPUNGED]'
-
-    return ProcessedData.Site(
-        name_str = f'{info.name} (ID: {info.id:03d})',
-        director_str = director_str,
-    )
-
-
 def mtf(info: Models.MTF) -> ProcessedData.MTF:
+    """
+    Processes MTF model for bar display
+
+    Parameters
+    ----------
+    info : Models.MTF
+        MTF model to process
+
+    Returns
+    -------
+    ProcessedData.MTF
+        Processed MTF data
+    """
+
+    # validation
+    if not isinstance(info, Models.MTF):
+        raise ArgumentError(info, 'Models.MTF')
+
     if info.site:
         site_name = f'Site-{info.site.id:03d}'
     else:
@@ -135,23 +156,75 @@ def mtf(info: Models.MTF) -> ProcessedData.MTF:
     else:
         leader_str = '[DATA EXPUNGED]'
 
-    active = 'Yes' if info.active else 'No'
+    active = 'Active' if info.active else 'Inactive'
 
-    name_str = f'MTF {info.name} "{info.nickname}" (MTF ID: {info.id})'
+    name_str = f'MTF {info.name} (ID\\: {info.id})'
 
     return ProcessedData.MTF(
         name_str = name_str,
+        nickname = f'"{info.nickname}"',
         site = site_name,
         leader_str = leader_str,
         active = active
     )
 
 
+def site(info: Models.Site) -> ProcessedData.Site:
+    """
+    Processes Site model for bar display
+
+    Parameters
+    ----------
+    info : Models.Site
+        Site model to process
+
+    Returns
+    -------
+    ProcessedData.Site
+        Processed Site data
+    """
+
+    # validation
+    if not isinstance(info, Models.Site):
+        raise ArgumentError(info, 'Models.Site')
+
+    if info.director:
+        director_str = (f'{info.director.name} '
+                        f'(ID: {info.director.id})')
+
+    else:
+        director_str = '[DATA EXPUNGED]'
+
+    return ProcessedData.Site(
+        name_str = f'{info.name} (ID\\: {info.id:03d})',
+        director_str = director_str,
+    )
+
+
 def user(info: Models.User) -> ProcessedData.User:
-    name_str = f'{info.title.name} {info.name} (ID: {info.id})'
+    """
+    Processes User model for bar display
+
+    Parameters
+    ----------
+    info : Models.User
+        User model to process
+
+    Returns
+    -------
+    ProcessedData.User
+        Processed User data
+    """
+
+    # validation
+    if not isinstance(info, Models.User):
+        raise ArgumentError(info, 'Models.User')
+
+    name_str = f'{info.title.name} {info.name} (ID\\: {info.id})'
 
     return ProcessedData.User(
         name_str = name_str,
         site = f'Site-{info.site_id:03d}',
-        clearance = info.clearance_lvl.name
+        clearance_str = info.clearance_lvl.name,
+        clearance_id = info.clearance_lvl.id
     )
