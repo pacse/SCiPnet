e4627f2c648b75428548cef45f7b8763528cc1f8
pacse <68709871+pacse@users.noreply.github.com>
2025-12-08 18:04:57 +1300

    Update lines.py
    It was in fact not finished 😢
    
    added func for default_formatting to save myself from the headaches of rich
    
    replaced colour with style, bc we not use rich style strings (eg 'cyan bold') as well as hex codes
    
    added colon escaping to _format_label_val_text using PLACEHOLDER const
    also allowed no colon formatting
    
    added format_centered_text as a helper for BarTemplate.render_top_line, print_piped_line & print_centered_line

--- CS50xFP/utils/display/core/bars/lines.py
+++ CS50xFP/utils/display/core/bars/lines.py
@@ -4,7 +4,9 @@ Helpers for printing piped lines and `print_piped_line()`
 
 # === Imports ===
 
-from ...config import BAR_WIDTH, LEFT_PADDING
+from ...config import (BAR_WIDTH, LEFT_PADDING, OTHER_CONT_CLASS,
+                       PLACEHOLDER, SPECIAL_TEXTS, ACTIVE_TEXT,
+                       DIGIT_REGEX, QUOTED_REGEX)
 from ....sql.exceptions import FieldError
 
 from rich.console import Console
@@ -15,59 +17,102 @@ from typing import Literal
 
 # === Helpers ===
 
+
 # Text formatting
-def _format_label_val_text(
-                           string: str,
-                           colour: str | None = None,
-                          ) -> tuple[Text, Text | str]:
+def default_formatting(string: str) -> Text:
     """
-    Formats text for piped line printing
+    Applies default formatting to string:
+    - digits are styled as 'cyan bold'
+    - text in double quotes is styled as 'green'
+    - special cases:
+      - '[DATA EXPUNGED]', 'None', & 'Inactive' are styled with OTHER_CONT_CLASS
+      - 'Active' is styled as 'green'
 
     Parameters
     ----------
     string : str
         Text to format
-    colour : str or None, optional
-        hex colour code or Rich colour name for text styling.
+
+    Returns
+    -------
+    Text
+        Formatted text
+    """
+    t = Text(string)
+
+    # special cases
+    if string.strip() in SPECIAL_TEXTS:
+        t.stylize(OTHER_CONT_CLASS)
+    elif string.strip() == ACTIVE_TEXT:
+        t.stylize('green')
+
+    # normal formatting
+    else:
+        t.highlight_regex(DIGIT_REGEX, 'cyan bold')  # digit coloring
+        t.highlight_regex(QUOTED_REGEX, 'green')     # text coloring
+
+    return t
+
+def _format_label_val_text(
+                           string: str,
+                           style: str | None = None,
+                          ) -> tuple[Literal[''] | Text | str, Text | str]:
+    """
+    Formats text for piped line printing
+
+    If string contains a colon, splits into label (bolded) and value (styled).
+
+    Parameters
+    ----------
+    string : str
+        Text to format
+    style : str or None, optional
+        string to pass to Text for styling.
         If None, uses default styling.
 
     Returns
     -------
-    Label : Text
-        Formatted label text
+    Label : Literal[''] or Text or str
+        Formatted label text (bolded), or empty string if no label, or string if brackets present.
     Value : Text or str
-        Formatted value text
-
-    Raises
-    ------
-    ValueError
-        If `string` does not contain a colon
+        Formatted value text. If no style provided, returns unstyled str.
+        ('[DATA EXPUNGED]', 'None', & 'Inactive' are styled with OTHER_CONT_CLASS.)
 
     Examples
     --------
     >>> _format_label_val_text('Label: Value', '#FF0000')
     (Text('Label:', style='bold'), Text(' Value', style='#FF0000'))
 
-    >>> _format_label_val_text('Label: Value')
-    (Text('Label:', style='bold'), ' Value')
+    >>> _format_label_val_text('Value', 'dim')
+    Text('Value', style='dim')
     """
-    # formatting only applies after the colon
-    split_string = string.split(':', 1)
 
-    # input validation
-    if len(split_string) != 2:
-        raise FieldError('string', string, 'Must contain a colon.')
+    # replace escaped colons
+    string = string.replace('\\:', PLACEHOLDER)
+
+    # formatting only applies after the colon
+    parts = string.split(':', 1)
+
+    # restore escaped colons
+    parts = [p.replace(PLACEHOLDER, ':') for p in parts]
+
+    # check str length
+    if len(parts) == 1:
+        if style:
+            return '', Text(parts[0], style)
+
+        else:
+            return '', default_formatting(parts[0])
+
 
     # first string is bold
-    str_1 = Text(f'{split_string[0]}:', 'bold')
+    str_1 = Text(f'{parts[0]}:', 'bold')
 
     # now second string
-    if split_string[1].strip() in ['[DATA EXPUNGED]', 'None']:
-        str_2 = Text(split_string[1], 'dim')
-    elif colour:
-        str_2 = Text(split_string[1], colour)
+    if style:
+        str_2 = Text(parts[1], style)
     else:
-        str_2 = split_string[1]
+        str_2 = default_formatting(parts[1])
 
     return (str_1, str_2)
 
@@ -116,7 +161,8 @@ def _calc_spacing(
         raise FieldError('side', side, "'l', 'r', or 'c'")
 
 
-    available_space = content_width - len(string)
+    # calculate available space without escape chars
+    available_space = content_width - len(string.replace('\\', ''))
 
     if available_space < 0:
         raise FieldError(
@@ -137,7 +183,7 @@ def _calc_spacing(
 # determine column seperators
 def _get_pipe_seps(cols: Literal[2, 3],
                    side: Literal['l', 'r', 'c']
-                  ) -> tuple[str, str]:
+                  ) -> tuple[Literal['║', '║║'], Literal['║', '']]:
     if cols == 2:
         left_sep  = '║' if side == 'l' else '║║'
     elif cols == 3:
@@ -153,15 +199,48 @@ def _get_pipe_seps(cols: Literal[2, 3],
 
 # === Main Functions ===
 
+def format_centered_text(
+                         string: str,
+                         side: Literal['l', 'r', 'c'],
+                         style: str | None = None,
+                         content_width: int = BAR_WIDTH,
+                         right_sep: Literal['║', ''] = '║'
+                        ) -> tuple[str, Literal[''] | Text | str,
+                                   Text | str, str, Literal['║','']]:
+    """
+    Formats centered text
+
+    Returns
+    -------
+    left_padding : str
+        Spaces before text
+    label_text : Text or Literal[''] or str
+        Formatted label text (bolded), or empty string if no label, or string if brackets present.
+    value_text : Text or str
+        Formatted value text. If no style provided, returns unstyled str.
+        ('[DATA EXPUNGED]', 'None', & 'Inactive' are styled with OTHER_CONT_CLASS.)
+    right_padding : str
+        Spaces after text
+    right_sep : Literal['║', '']
+        Right separator
+    """
+
+    left_padding, right_padding = \
+        _calc_spacing(string, content_width, side)
+    label_text, value_text = _format_label_val_text(string, style)
+
+    return left_padding, label_text, value_text, right_padding, right_sep
+
+
 def print_centered_line(console: Console,
                         string: str,
                         side: Literal['l', 'r', 'c'],
-                        colour: str | None = None,
+                        style: str | None = None,
                         content_width: int = BAR_WIDTH,
                        ) -> None:
     """
     Renders a formatted centered line to the console
-    String must contain a colon and fit in content width
+    String must fit in content width
 
     Parameters
     ----------
@@ -175,8 +254,8 @@ def print_centered_line(console: Console,
         - 'l': left side
         - 'r': right side
         - 'c': center
-    colour : str or None, optional
-        hex colour code or Rich colour name for text styling.
+    style : str or None, optional
+        string to use for text styling.
         If None, uses default styling.
     content_width : int, default=BAR_WIDTH
         Width of the line content area
@@ -185,7 +264,6 @@ def print_centered_line(console: Console,
     ------
     ValueError
         - If `side` is not 'l', 'r', or 'c'
-        - If `string` does not contain a colon
         - If `string` is too long to fit in content width
 
     Example
@@ -194,16 +272,15 @@ def print_centered_line(console: Console,
     '        Label: Value        '  # printed to console
     """
 
-    # calculate spacing
-    left_padding, right_padding = _calc_spacing(string, content_width, side)
-
-    # format text
-    label_text, value_text = _format_label_val_text(string, colour)
-
-    # print line
+    # Generate and render line
     console.print(
-                  left_padding, label_text,
-                  value_text, right_padding,
+                  *format_centered_text(
+                                        string,
+                                        side,
+                                        style,
+                                        content_width,
+                                        '' # no right sep
+                                       ),
                   end='', sep=''
                  )
 
@@ -211,13 +288,13 @@ def print_centered_line(console: Console,
 def print_piped_line(console: Console,
                      string: str,
                      side: Literal['l', 'r', 'c'],
-                     colour: str | None = None,
+                     style: str | None = None,
                      content_width: int = BAR_WIDTH,
                      cols: Literal[2, 3] = 2
                     ) -> None:
     """
     Renders a piped, formatted centered line to the console
-    String must contain a colon and fit in content width
+    String must fit in content width
 
     Parameters
     ----------
@@ -230,8 +307,8 @@ def print_piped_line(console: Console,
         - 'l': left side
         - 'r': right side
         - 'c': center
-    colour : str or None, optional
-        hex colour code or Rich colour name for text styling.
+    style : str or None, optional
+        string to use for text styling.
         If None, uses default styling.
     content_width : int, default=BAR_WIDTH
         Width of the line content area
@@ -244,7 +321,6 @@ def print_piped_line(console: Console,
     ------
     ValueError
         - If `side` is not 'l', 'r', or 'c'
-        - If `string` does not contain a colon
         - If `string` is too long to fit in content width
         - If `cols` is not 2 or 3
         - if `side` is 'c' and `cols` is 2
@@ -264,11 +340,14 @@ def print_piped_line(console: Console,
     if side == 'l':
         left_sep = f'{LEFT_PADDING}{left_sep}'
 
-    print(left_sep, end='')
-
-    print_centered_line(
-                        console, string, side,
-                        colour, content_width
-                       )
-
-    print(right_sep, end = ('\n' if side == 'r' else ''))
+    console.print(
+                  left_sep,
+                  *format_centered_text(
+                                        string,
+                                        side,
+                                        style,
+                                        content_width,
+                                        right_sep
+                                       ),
+                  sep='', end = ('\n' if side == 'r' else '')
+                 )
