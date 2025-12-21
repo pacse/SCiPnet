3c51f9f7a35bf3dc81fa34a13859997cf973cb1f
pacse <68709871+pacse@users.noreply.github.com>
2025-12-22 10:54:39 +1300

    Delete exceptions.py
    moved to general

---
+++ CS50xFP/utils/general/__init__.py
@@ -0,0 +1,9 @@
+"""
+Config and shared files for the utils module
+
+Contains
+--------
+- Display configuration (display_config.py)
+- SQLite configuration (sql_config.py)
+- Custom exceptions (exceptions.py)
+"""
